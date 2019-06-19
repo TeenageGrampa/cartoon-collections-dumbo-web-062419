bfa55b015d84ecf916fe5f2b22363a31e740ecbf
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-19 16:25:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet(calls)
+def summon_captain_planet(planeteer_calls)
   calls.collect do |call|
     call.capitalize << "!"
   end
